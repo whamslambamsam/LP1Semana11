b78390e34b67e66468411a2240ecbb82b40ab3f3
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-19 11:43:16 +0100

    Added PlayerManagerMVC project

---
+++ PlayerManagerMVC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerManagerMVC
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
