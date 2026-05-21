df7a622d90975e366684b57684dfabf6c10547dd
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-21 16:57:05 +0100

    Simplified and shortened during MVC formating

--- StringGenerator/Program.cs
+++ StringGenerator/Program.cs
@@ -3,34 +3,13 @@ using System.Text;
 
 namespace StringGenerator
 {
-
-    public class Generator
-    {
-        private const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-        public static string Generate(int seed, int length = 16)
-        {
-            Random rng = new Random(seed);
-
-            StringBuilder result = new StringBuilder(length);
-
-            for (int i = 0; i < length; i++)
-            {
-                int index = rng.Next(chars.Length);
-                result.Append(chars[index]);
-            }
-
-            return result.ToString();
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             int seed = int.Parse(args[0]);
 
-            string output = Generator.Generate(seed);
+            string output = Controller.Generate(seed);
 
             Console.WriteLine(output);
         }
