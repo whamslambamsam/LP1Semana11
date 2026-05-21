13513a502b2715cb08dab067be266b2fdcc814cd
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-21 16:20:52 +0100

    Added files from moodle

--- StringGenerator/Program.cs
+++ StringGenerator/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Text;
 
 namespace StringGenerator
 {
-    public class Program
+
+    public class Generator
     {
-        private static void Main(string[] args)
+        private const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public static string Generate(int seed, int length = 16)
         {
-            Console.WriteLine("Hello LP!");
+            Random rng = new Random(seed);
+
+            StringBuilder result = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rng.Next(chars.Length);
+                result.Append(chars[index]);
+            }
+
+            return result.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seed = int.Parse(args[0]);
+
+            string output = Generator.Generate(seed);
+
+            Console.WriteLine(output);
         }
     }
 }
