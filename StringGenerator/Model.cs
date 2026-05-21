12a8918245c6aea8a599f4faf9f45377f990a212
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-21 16:34:55 +0100

    Placed chars string on Model.cs

---
+++ StringGenerator/Model.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Text;
+
+namespace StringGenerator
+{
+    public class Model
+    {
+        public const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    }
+}
