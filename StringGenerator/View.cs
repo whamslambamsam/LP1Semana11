2647884dab720174e66165edfbf8cd36c8712233
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-21 16:57:28 +0100

    Minor changes so that it can be treated as its own class

---
+++ StringGenerator/View.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO.Pipelines;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace StringGenerator
+{
+    public class View
+    {
+        
+    }
+}
