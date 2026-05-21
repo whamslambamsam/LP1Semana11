1ad020546d0745cdd552fa69aa065cc9503bc62c
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-21 15:58:41 +0100

    Created StringGenerator project

---
+++ StringGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringGenerator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
